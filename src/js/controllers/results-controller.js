273b470e429490943c79fd276c5a13aa937fb925
Maximilian Schmitt <maximilian.schmitt@stud.uni-regensburg.de>
2014-08-28 14:40:57 +0200

    highlight quick link

--- src/js/controllers/results-controller.js
+++ src/js/controllers/results-controller.js
@@ -1,11 +1,21 @@
 'use strict';
 
+var quickLinksIndex = {
+	'grips': ['GRIPS', 'e-learning', 'elearning', 'g.r.i.p.s', 'e learning'],
+	'lsf': [ 'vorlesungsverzeichnis', 'stundenplan', 'vorlesungen', 'veranstaltungen', 'kurse'],
+	'k-laufwerk': ['k laufwerk', 'netstorage', 'kurslaufwerk', 'kurssoft', 'k drive','k-drive', 'laufwerk k', 'k server','kurs laufwerk', 'net storage' , 'klaufwerk', 'kdrive'],
+	'mail': ['e-mail', 'email', 'group wise', 'uni mail',  'unimail', 'groupwise'],
+	'flexnow': ['prüfungsergebnisse', 'noten', 'studentendaten', 'prüfungsanmeldung']
+};
+
 var ResultsController = function($scope, $stateParams, $state, solrApi) {
 	$scope.searchText     = $stateParams.query;
 	$scope.page           = $stateParams.page ? Math.max(parseInt($stateParams.page, 10), 1) : 1;
 
 	$scope.resultsPerPage = 10;
 	$scope.visiblePages   = 15;
+
+	$scope.quickLinkSuggestion = getQuickLinkSuggestion($scope.searchText);
 
 	$scope.pagingStart = function() {
 		return Math.max(0, $scope.page - 1 - Math.floor($scope.visiblePages / 2));
@@ -24,6 +34,22 @@ var ResultsController = function($scope, $stateParams, $state, solrApi) {
 		$scope.results = response.results;
 		$scope.spellSuggestion = response.spellSuggestion;
 	});
+
+	function getQuickLinkSuggestion(searchText) {
+		for (var quickLink in quickLinksIndex) {
+			if (quickLink === searchText) {
+				return quickLink;
+			}
+
+			var synonyms = quickLinksIndex[quickLink];
+
+			for (var i = 0; i < synonyms.length; i++) {
+				if (searchText === synonyms[i]) {
+					return quickLink;
+				}
+			}
+		}
+	}
 };
 
 module.exports = ResultsController;
